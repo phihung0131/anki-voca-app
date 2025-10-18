5167b9ce70adfa307fc46401870c5d6bebd36488
phihung0131 <phihung0131@gmail.com>
2025-10-18 23:57:46 +0700

    asd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17"
+    id("com.google.devtools.ksp") version "2.0.20-1.0.24"
 }
 
 android {
